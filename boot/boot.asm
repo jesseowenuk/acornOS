6475df14b8e5bebfd27b1688c63ce7191d7fca93
Jesse Owen <jesseowenuk@googlemail.com>
2026-05-12 20:19:28 +0100

    feat: print first character via BIOS interrupt (INT 0x10)

--- boot/boot.asm
+++ boot/boot.asm
@@ -2,6 +2,10 @@
 [ORG 0x7C00]            ; Tell NASM our code will be loaded at 0x7C00
 
 start:
+    mov ah, 0x0E        ; BIOS teletype function
+    mov al, 'A'         ; Character to print
+    int 0x10            ; Call BIOS video interrupt
+    
     jmp $               ; Infinite loop - hang here forever
 
 ; Pad the file to 510 bytes, then add the 2 byte boot signature
