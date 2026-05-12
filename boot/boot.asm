2f9db4a149b0729d590c8b99a3e975fb1bd6cdab
Jesse Owen <jesseowenuk@googlemail.com>
2026-05-12 20:24:12 +0100

    feat: print full string via BIOS interrupt loop

--- boot/boot.asm
+++ boot/boot.asm
@@ -2,11 +2,25 @@
 [ORG 0x7C00]            ; Tell NASM our code will be loaded at 0x7C00
 
 start:
-    mov ah, 0x0E        ; BIOS teletype function
-    mov al, 'A'         ; Character to print
-    int 0x10            ; Call BIOS video interrupt
-    
+    mov si, msg         ; Point SI register at our message
+    call print          ; Call our print routine
+
     jmp $               ; Infinite loop - hang here forever
+
+print:
+    mov ah, 0x0E        ; BIOS teletype function
+.loop:
+    lodsb               ; Load byte at [SI] into AL, then increment SI
+    cmp al, 0           ; Is it the null terminator?
+    je .done            ; If yes, we're finished
+    int 0x10            ; Otherwise, print the character
+    jmp .loop           ; And loop
+
+.done: 
+    ret
+
+msg db 'Hello from AcornOS!', 13, 10, 0
+; 13 = carriage return, 10 = newline, 0 = null terminator
 
 ; Pad the file to 510 bytes, then add the 2 byte boot signature
 times 510-($-$$) db 0
