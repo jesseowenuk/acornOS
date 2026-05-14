859313cc91fbb7a95bb266612d8d0eda327319ea
Jesse Owen <jesseowenuk@googlemail.com>
2026-05-14 21:08:22 +0100

    fix: pad disk image to 1.44MB floppy size to prevent INT 0x13 overread

--- Makefile
+++ Makefile
@@ -1,12 +1,23 @@
 .PHONY: all run clean
 
-all: boot.bin
+CC = i686-elf-gcc
+CFLAGS = -ffreestanding -O2 -Wall -Wextra -fno-builtin
+
+all: os.img
 
 boot.bin: boot/boot.asm
-    nasm -f bin boot/boot.asm -o boot.bin
+	nasm -f bin boot/boot.asm -o boot.bin
 
-run: boot.bin
-    qemu-system-i386 -drive format=raw,file=boot.bin
+kernel.bin: kernel/kernel.c
+	$(CC) $(CFLAGS) -c kernel/kernel.c -o kernel.o
+	i686-elf-ld -o kernel.bin -Ttext 0x1000 --oformat binary kernel.o
+
+os.img: boot.bin kernel.bin
+	cat boot.bin kernel.bin > os.img
+	truncate -s 1440k os.img
+
+run: os.img
+	qemu-system-i386 -drive format=raw,file=os.img
 
 clean:
-    rm -f *.bin
+	rm -f *.bin *.o *.img
--- boot/boot.asm
+++ boot/boot.asm
@@ -1,7 +1,11 @@
 [BITS 16]               ; Tell NASM we're writing 16-bit code
 [ORG 0x7C00]            ; Tell NASM our code will be loaded at 0x7C00
 
+KERNEL_OFFSET equ 0x1000    ; Where we'll load the kernel in memory
+
 start:
+    mov [boot_drive], dl    ; BIOS puts boot drive in DL - save it immediatley
+
     ; Segment register setup - zero them all out
     xor ax, ax          ; AX = 0 (xor is the idiomatic way to zero a register)
     mov ds, ax          ; Data segment = 0
@@ -14,8 +18,10 @@ start:
     mov al, 0x03        ; Mode 3 = 80x25 colour text
     int 0x10
 
-    mov si, msg         ; Point SI register at our message
+    mov si, msg_load    ; Point SI register at our message
     call print          ; Call our print routine
+
+    call load_kernel    ; Load kernel from disk
 
     ; Disable interrupts before switching modes
     cli
@@ -31,6 +37,7 @@ start:
     ; Far jump to flush the CPU pipeline and land in 32-bit mode
     jmp 0x08:protected_mode
 
+; --- Print routine -----------------------------------------------------
 print:
     mov ah, 0x0E        ; BIOS teletype function
 .loop:
@@ -43,7 +50,32 @@ print:
 .done: 
     ret
 
-msg db 'Switching to protected mode...', 13, 10, 0
+; --- Disk load -----------------------------------------------------
+load_kernel:
+    mov si, msg_disk
+    call print
+
+    mov ah, 0x02        ; BIOS read sectors function
+    mov al, 5          ; Number of sectors to read
+    mov ch, 0           ; Cylinder 0
+    mov cl, 2           ; Start from sector 2 (sector 1 is our bootloader)
+    mov dh, 0           ; Head 0
+    mov dl, [boot_drive]    ; Use the saved drive number
+    mov bx, KERNEL_OFFSET   ; Load into memory at 0x1000
+    int 0x13
+    jc disk_error       ; Jump if carry flag set (error)
+    ret
+
+disk_error:
+    mov si, msg_error
+    call print
+    jmp $
+
+boot_drive db 0 ; 1 byte to store the drive 
+
+msg_load db 'acornOS booting...', 13, 10, 0
+msg_disk db 'Loading kernel from disk...', 13, 10, 0
+msg_error db 'Disk read error!', 13, 10, 0
 ; 13 = carriage return, 10 = newline, 0 = null terminator
 
 ; --- GDT -----------------------------------------------------
@@ -87,11 +119,7 @@ protected_mode:
     mov gs, ax
     mov esp, 0x90000            ; Set up a fresh 32-bit stack
 
-    ; Confirm we made it - write directly to VGA memory
-    mov byte [0xB8000], 'O'
-    mov byte [0xB8001], 0x0A    ; Green on black
-    mov byte [0xB8002], 'K'
-    mov byte [0xB8003], 0x0A    ; Green on black
+    call KERNEL_OFFSET          ; jump to the kernel
 
     jmp $               ; Infinite loop - hang here forever
 
